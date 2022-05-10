ab1a2c334c03c7e1f3766a21f08d41f1313f9532
bbubbush <bbubbush@gmail.com>
2022-05-10 22:56:00 +0900

    init project

---
+++ src/test/java/com/bbubbush/board/SpringBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbubbush.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
