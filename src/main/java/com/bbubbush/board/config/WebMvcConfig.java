6d30a0fd907f5a9c7182409fd0b1eb78e9d24a11
bbubbush <bbubbush@gmail.com>
2022-05-22 21:52:47 +0900

    Feat: api 호출시, 요청정보를 로깅
    인터셉터는 HandlerInterceptor를 구현하여 만들었고, 이를 WebMvcConfig에 등록하여 appInterceptor 처리함
    대상 URI는 "/board/**"로 설정

---
+++ src/main/java/com/bbubbush/board/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.bbubbush.board.config;
+
+import com.bbubbush.board.interceptor.PreLoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  private final PreLoggingInterceptor preLoggingInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(preLoggingInterceptor)
+      .addPathPatterns("/board/**");
+  }
+}
