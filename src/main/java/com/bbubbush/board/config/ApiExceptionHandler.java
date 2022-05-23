e0fb6a1c24fcb28998e4fdcfe3bb90f6940fcc24
bbubbush <bbubbush@gmail.com>
2022-05-23 10:46:59 +0900

    Feat: Api 예외처리 핸들러 추가

---
+++ src/main/java/com/bbubbush/board/config/ApiExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.bbubbush.board.config;
+
+import com.bbubbush.board.exception.ArticleNotFoundException;
+import com.bbubbush.board.exception.CommonApiException;
+import com.bbubbush.board.exception.NotModifiedDataException;
+import com.bbubbush.board.type.ApiResponseType;
+import com.bbubbush.board.util.ApiResponse;
+import com.bbubbush.board.vo.common.ResponseVO;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+  @ExceptionHandler(ArticleNotFoundException.class)
+  public <T> ResponseVO articleNotFoundExceptionHandler(ArticleNotFoundException e) {
+    return ApiResponse.error(ApiResponseType.ARTICLE_NOT_FOUND.getCode(), e.getMessage());
+  }
+
+  @ExceptionHandler(NotModifiedDataException.class)
+  public <T> ResponseVO notModifiedDataExceptionHandler(NotModifiedDataException e) {
+    return ApiResponse.error(ApiResponseType.NOT_MODIFIED_DATA.getCode(), e.getMessage());
+  }
+
+  @ExceptionHandler(CommonApiException.class)
+  public <T> ResponseVO commonApiExceptionHandler(CommonApiException e) {
+    return ApiResponse.error(ApiResponseType.SERVER_ERROR.getCode(), e.getMessage());
+  }
+
+  @ExceptionHandler(Exception.class)
+  public <T> ResponseVO exceptionHandler() {
+    return ApiResponse.error(ApiResponseType.SERVER_ERROR);
+  }
+
+}
